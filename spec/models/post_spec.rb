9d8634589e63e11ef66f6600e81c68557349f96a
Seyi Oyemade <seyioyemade918@gmail.com>
2023-07-15 12:56:28 +0100

    Add test for five_most_recent_comments method in the Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 RSpec.describe Post, type: :model do
+  describe '#five_most_recent_comments method' do
+    let(:user) { create(:user) }
+    it 'it returns the last 5 comments of a post' do
+      user = User.create(name: 'Seyi', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Germany.')
+      post = Post.create(author_id: user.id, title: 'Hello', text: 'This is my first post')
+
+      first_comment = Comment.create(author_id: user.id, post_id: post.id, text: 'text')
+      Comment.create(author_id: user.id, post_id: post.id, text: 'text')
+      Comment.create(author_id: user.id, post_id: post.id, text: 'text')
+      Comment.create(author_id: user.id, post_id: post.id, text: 'text')
+      Comment.create(author_id: user.id, post_id: post.id, text: 'text')
+      Comment.create(author_id: user.id, post_id: post.id, text: 'text')
+
+      five_most_recent_comments = post.five_most_recent_comments
+
+      expect(five_most_recent_comments).to_not include(first_comment)
+    end
+  end
+  describe '#update_posts_counter method' do
+  let(:user) { create(:user) }
+    it 'it increments the posts_counter of a user' do
+      user = User.create(name: 'Seyi', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                        bio: 'Teacher from Germany.', posts_counter: 1)
+      Post.create(author_id: user.id, title: 'Hello', text: 'This is my first post')
+
+      expect(user.posts_counter).to eql 1
+    end
+  end
   describe 'validations' do
     it 'requires a title' do
       post = Post.new(title: nil)
