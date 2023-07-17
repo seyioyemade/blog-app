30416b3ff390bb04051a53c2097242cca4ee6d39
Seyi Oyemade <seyioyemade918@gmail.com>
2023-07-17 23:33:50 +0100

    Add routes and create controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def post1
+  end
+
+  def post2
+  end
+
+  def post3
+  end
+
+  def post4
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'users#post1'
+  get "/users/745", to: "users#post2"
+  get "/users/745/posts", to: "users#post4"
+  get "/users/745/posts/3", to: "users#post4"
 end
